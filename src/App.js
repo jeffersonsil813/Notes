2b85956fc1c6a5c77537553694f68aca30b633e7
Jefferson Santos <49699985+jeffersonsil813@users.noreply.github.com>
2021-07-03 00:30:44 -0300

    Adding Validations

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ function modal(error) {
 // ======= PREVENT DEFAULT ======= \\
 document.querySelector("#new-note").addEventListener("click", function(event){
     event.preventDefault()
-  })
+})
 
 document.querySelector(".modal a").addEventListener("click", function(event){
     event.preventDefault()
-  })
+})
+
+// ======= ON CHANGE INPUTS ======= \\
+function onChangeInput() {
+document.querySelector('#btn-options').children[1].classList.remove('disable')
+}
 
 // ======= DATETIME ======= \\
 function dateTime() {
@@ -38,6 +43,9 @@ const Main = {
         github.innerHTML = `
         <img src="assets/notes/github.png" alt="github-icon" /> Jefferson Santos
         `
+        github.setAttribute('title', 'Github')
+        // github.setAttribute('href', 'https://github.com/jeffersonsil813')
+        github.setAttribute('id', 'acessGithub')
         github.setAttribute('onmouseover', 'this.children[0].src = "assets/notes/github2.png"')
         github.setAttribute('onmouseout', 'this.children[0].src = "assets/notes/github.png"')
 
@@ -49,26 +57,26 @@ const Main = {
         const html = `
             <header>
                 <div id="about-note">
-                    <input type="text" minlength="5" autocomplete="off" value="${index == null? '' : Notes.all[index].title}" placeholder="Título" maxlength="30" id="edit-title"/>
+                    <input type="text" onkeydown="onChangeInput()" onpaste="onChangeInput()" oninput="onChangeInput()" minlength="5" autocomplete="off" value="${index == null? '' : Notes.all[index].title}" placeholder="Título" maxlength="30" id="edit-title"/>
                     <span tabindex="-1">${index == null? '' : Notes.all[index].datetime}</span>
                     
                     <div id="radio-area">
                         <span>Prioridade:</span>
-                        <input tabindex="-1" id="low" name="radio" value="Baixa" type="radio" /> <label for="low">Baixa</label>
+                        <input tabindex="-1" id="low" name="radio" onchange="onChangeInput()" value="Baixa" type="radio" /> <label for="low">Baixa</label>
 
-                        <input tabindex="-1" id="medium" name="radio" value="Média" type="radio" /> <label for="medium">Média</label>
+                        <input tabindex="-1" id="medium" name="radio" onchange="onChangeInput()" value="Média" type="radio" /> <label for="medium">Média</label>
 
-                        <input tabindex="-1" id="high" name="radio" value="Alta" type="radio" /> <label for="high">Alta</label>
+                        <input tabindex="-1" id="high" name="radio" onchange="onChangeInput()" value="Alta" type="radio" /> <label for="high">Alta</label>
                     </div>
                 </div>
 
                 <div id="btn-options">
                     <button tabindex="-1" class="btn" title="Cancelar" onclick="Main.editMessage()">Cancelar</button>
-                    <button tabindex="-1" class="btn" title="Salvar" onclick="Form.save(${index})">Salvar</button>
+                    <button tabindex="-1" class="btn disable" title="Salvar" onclick="Form.save(${index})">Salvar</button>
                 </div>
             </header>
 
-            <textarea tabindex="0" name="edit-content" minlength="10" id="edit-content" placeholder="Digite suas anotações">${index == null? '' : Notes.all[index].content}</textarea>
+            <textarea tabindex="0" name="edit-content" minlength="10" onkeydown="onChangeInput()" onpaste="onChangeInput()" oninput="onChangeInput()" id="edit-content" placeholder="Digite suas anotações">${index == null? '' : Notes.all[index].content}</textarea>
         `
         return html
     },
@@ -111,7 +119,7 @@ const Form = {
     getValues() {
         return {
             title: document.querySelector('input#edit-title').value != ''? 
-            (document.querySelector('input#edit-title').value[0].toUpperCase() + document.querySelector('input#edit-title').value.substring(1))
+            (document.querySelector('input#edit-title').value[0].toUpperCase() + document.querySelector('input#edit-title').value.substring(1)).trim()
             : '',
             datetime: dateTime(),
             content: document.querySelector('textarea#edit-content').value,
@@ -137,11 +145,11 @@ const Form = {
 
     validateFields() {
         const {title, datetime, content, priority} = Form.getValues()
-        if(title.trim() === "" || datetime.trim() === "" || content.trim() === "") {
+        if(title === "" || datetime === "" || content === "") {
             throw new Error("Por favor, preencha todos os campos!")
-        } else if(title.trim().length < 5) {
+        } else if(title.length < 5) {
             throw new Error('O campo título deve conter ao menos 5 caracteres!')
-        } else if(content.trim().length < 10) {
+        } else if(content.length < 10) {
             throw new Error('O campo de anotações deve conter ao menos 10 caracteres!')
         } else if(priority === '') {
             throw new Error('Por favor, selecione um nível de prioridade!')
@@ -168,10 +176,20 @@ const Notes = {
     all: Storage.get(),
 
     add(index, note) {
+        // index != null -> estou editando uma nota
         if(index != null) {
+            // remove uma nota do array/banco de acordo com o index
             Notes.all.splice(index, 1)
             Notes.all.unshift(note)
         } else {
+            // else -> adicionando nova nota
+            const {title} = Form.getValues()
+            for(e of Notes.all) {
+                if(title.toLowerCase() === e.title.toLowerCase()) {
+                    throw new Error('Já existe uma nota com esse título! Por favor, faça uma alteração.')
+                }
+            }
+
             // unshift adiciona o elemento na posição [0] do array. O shift remove o elemento [0]
             Notes.all.unshift(note)
         }
